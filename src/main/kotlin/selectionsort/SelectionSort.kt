fc6ec5c460d27f5b200cd0b6c98eeb979563857c
James <jamesdudhiprice@gmail.com>
2024-04-25 10:34:33 +0100

    Selection sort update

--- src/main/kotlin/selectionsort/SelectionSort.kt
+++ src/main/kotlin/selectionsort/SelectionSort.kt
@@ -34,7 +34,7 @@ private fun findSmallestIndexInList(list: List<Int>): Int {
  * 1) Selection sort takes in an unsorted mutable array like an ArrayList
  * 2) We create an empty array in the function body
  * 3) We loop over each item in the array passed into the function
- * 4) We use a for loop to find the smallest index in the list passed in
+ * 4) We use a for loop to find the smallest item and its index in the list passed in
  * 5) We add the smallest item in the list to our new empty list
  * 6) We do this for the entire list
  * 7) This is log n squared at worst
--- src/main/kotlin/selectionsort/readme.md
+++ src/main/kotlin/selectionsort/readme.md
@@ -5,6 +5,14 @@
 
 ### <ins>Explanation</ins>
 
+1) Selection sort takes in an unsorted mutable array like an ArrayList
+2) We create an empty array in the function body
+3) We loop over each item in the array passed into the function
+4) We use a for loop to find the smallest item and its index in the list passed in
+5) We add the smallest item in the list to our new empty list
+6) We do this for the entire list
+7) This is log<sub>2</sub>n<sup>2</sup> at worst
+
 
 ### <ins>Visual</ins>
 ![selection-sort.png](selection-sort.png)
